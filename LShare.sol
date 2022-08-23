fad6d9ec180ca7808c92113a8a20202355bce111
Simba <Simbaroaringlion@gmail.com>
2022-08-23 22:24:39 +0200

    adding released contracts

---
+++ LShare.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+                                                                                        
+/***
+ *     ██▀███   ▒█████   ▄▄▄       ██▀███   ██▓ ███▄    █   ▄████  ██▓     ██▓ ▒█████   ███▄    █ 
+ *    ▓██ ▒ ██▒▒██▒  ██▒▒████▄    ▓██ ▒ ██▒▓██▒ ██ ▀█   █  ██▒ ▀█▒▓██▒    ▓██▒▒██▒  ██▒ ██ ▀█   █ 
+ *    ▓██ ░▄█ ▒▒██░  ██▒▒██  ▀█▄  ▓██ ░▄█ ▒▒██▒▓██  ▀█ ██▒▒██░▄▄▄░▒██░    ▒██▒▒██░  ██▒▓██  ▀█ ██▒
+ *    ▒██▀▀█▄  ▒██   ██░░██▄▄▄▄██ ▒██▀▀█▄  ░██░▓██▒  ▐▌██▒░▓█  ██▓▒██░    ░██░▒██   ██░▓██▒  ▐▌██▒
+ *    ░██▓ ▒██▒░ ████▓▒░ ▓█   ▓██▒░██▓ ▒██▒░██░▒██░   ▓██░░▒▓███▀▒░██████▒░██░░ ████▓▒░▒██░   ▓██░
+ *    ░ ▒▓ ░▒▓░░ ▒░▒░▒░  ▒▒   ▓▒█░░ ▒▓ ░▒▓░░▓  ░ ▒░   ▒ ▒  ░▒   ▒ ░ ▒░▓  ░░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ 
+ *      ░▒ ░ ▒░  ░ ▒ ▒░   ▒   ▒▒ ░  ░▒ ░ ▒░ ▒ ░░ ░░   ░ ▒░  ░   ░ ░ ░ ▒  ░ ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░
+ *      ░░   ░ ░ ░ ░ ▒    ░   ▒     ░░   ░  ▒ ░   ░   ░ ░ ░ ░   ░   ░ ░    ▒ ░░ ░ ░ ▒     ░   ░ ░ 
+ *       ░         ░ ░        ░  ░   ░      ░           ░       ░     ░  ░ ░      ░ ░           ░ 
+ *  
+ *  https://www.roaringlion.xyz/
+ */
+
+
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./owner/Operator.sol";
+
+contract LShare is ERC20Burnable, Operator {
+    using SafeMath for uint256;
+
+    // TOTAL MAX SUPPLY = 83333
+    uint256 public constant FARMING_POOL_REWARD_ALLOCATION = 50000 ether;
+    uint256 public constant DAO_FUND_POOL_ALLOCATION = 33333 ether;
+
+    uint256 public constant VESTING_DURATION = 731 days; // 2 years
+    uint256 public startTime;
+    uint256 public endTime;
+
+    uint256 public daoFundRewardRate;
+    address public daoFund;
+
+    uint256 public lastClaimedTime;
+
+    bool public rewardPoolDistributed = false;
+
+    uint256 private totalBurned_;
+
+    constructor(
+        uint256 _startTime,
+        address _daoFund
+    ) public ERC20("Lion Share", "LSHARE") {
+        _mint(msg.sender, 1 ether); // mint 1 LSHARE for initial liquidity deployment
+
+        startTime = _startTime; 
+        endTime = startTime + VESTING_DURATION;
+
+        lastClaimedTime = startTime;
+
+        daoFundRewardRate = DAO_FUND_POOL_ALLOCATION.div(VESTING_DURATION);
+
+        require(_daoFund != address(0), "Address cannot be 0");
+        daoFund = _daoFund;
+    }
+
+    function setDaoFund(address _daoFund) external onlyOperator {
+        require(_daoFund != address(0), "zero");
+        daoFund = _daoFund;
+    }
+
+    function unclaimedDaoFund() public view returns (uint256 _pending) {
+        uint256 _now = block.timestamp;
+        if (_now > endTime) _now = endTime;
+        if (lastClaimedTime >= _now) return 0;
+        _pending = _now.sub(lastClaimedTime).mul(daoFundRewardRate);
+    }
+
+    /**
+     * @dev Claim pending rewards to community
+     */
+    function claimRewards() external {
+        uint256 _pending = unclaimedDaoFund();
+        if (_pending > 0 && daoFund != address(0)) {
+            _mint(daoFund, _pending);
+        }
+        lastClaimedTime = block.timestamp;
+    }
+
+    /**
+     * @notice distribute to reward pool (only once)
+     */
+    function distributeReward(address _farmingIncentiveFund)
+        external
+        onlyOperator
+    {
+        require(!rewardPoolDistributed, "only can distribute once");
+        require(_farmingIncentiveFund != address(0), "!_farmingIncentiveFund");
+        rewardPoolDistributed = true;
+        _mint(_farmingIncentiveFund, FARMING_POOL_REWARD_ALLOCATION);
+    }
+
+    function burn(uint256 amount) public override {
+        super.burn(amount);
+    }
+
+    function totalBurned() external view returns (uint256) {
+        return totalBurned_;
+    }
+
+    function _burn(address _account, uint256 _amount) internal override {
+        super._burn(_account, _amount);
+        totalBurned_ = totalBurned_.add(_amount);
+    }
+
+    function governanceRecoverUnsupported(
+        IERC20 _token,
+        uint256 _amount,
+        address _to
+    ) external onlyOperator {
+        _token.transfer(_to, _amount);
+    }
+}
